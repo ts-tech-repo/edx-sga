3c033aff77d8a8bf361787867fb5b0e19b787818
rajeshk.g <rajesh.g@talentsprint.com>
2025-03-24 17:30:40 +0530

    Method name changed in finalize-upload on click function.

--- edx_sga/static/js/src/edx_sga.js
+++ edx_sga/static/js/src/edx_sga.js
@@ -36,7 +36,7 @@ function StaffGradedAssignmentXBlock(runtime, element) {
       var content = $(element).find('#sga-content').html(template(state));
 
       $(content).find('.finalize-upload').on('click', function () {
-        $.post(finalizeUploadUrl).success(
+        $.post(finalizeUploadUrl).done(
           function (state) {
             state.uploadSuccess = true;
             render(state);
