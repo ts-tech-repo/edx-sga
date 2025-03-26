58dd7ee50cb33c1979f79b7e2d23e89babe73f6b
rajeshk.g <rajesh.g@talentsprint.com>
2025-03-26 12:04:37 +0530

    Make changes releted to downloadSubmission file in SGA.

--- edx_sga/static/js/src/edx_sga.js
+++ edx_sga/static/js/src/edx_sga.js
@@ -348,10 +348,11 @@ function StaffGradedAssignmentXBlock(runtime, element) {
         $(element).find('#download-init-button').click(function (e) {
           e.preventDefault();
           var self = this;
+          $(self).addClass("disabled");
           $.get(prepareDownloadSubmissionsUrl).then(
             function (data) {
               if (data["downloadable"]) {
-                window.location = downloadSubmissionsUrl;
+                window.open(downloadSubmissionsUrl, '_blank');
                 $(self).removeClass("disabled");
               } else {
                 $(self).addClass("disabled");
