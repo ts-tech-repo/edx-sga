f395afef15d1ceec5c897da5a71ebd2df680c8ce
rajeshk.g <rajesh.g@talentsprint.com>
2025-04-02 18:04:09 +0530

    code fixes for download Submission files.

--- edx_sga/static/js/src/edx_sga.js
+++ edx_sga/static/js/src/edx_sga.js
@@ -41,6 +41,7 @@ function StaffGradedAssignmentXBlock(runtime, element) {
             state.uploadSuccess = true;
             render(state);
             $(element).find('#download-init-button').removeClass('disabled');
+            pollSubmissionDownload();
           }
         ).fail(
           function () {
