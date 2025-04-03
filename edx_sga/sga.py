de0a924d98328ceb987cfcd1c69178362e0d9611
rajeshk.g <rajesh.g@talentsprint.com>
2025-04-03 12:06:04 +0530

    changed the downloadable value to true.

--- edx_sga/sga.py
+++ edx_sga/sga.py
@@ -531,7 +531,7 @@ class StaffGradedAssignmentXBlock(
                 self.block_course_id, self.block_id, location, user.username
             )
 
-        return Response(json_body={"downloadable": zip_file_ready})
+        return Response(json_body={"downloadable": True})
 
     @XBlock.handler
     def download_submissions(
